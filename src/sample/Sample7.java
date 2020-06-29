3e78627f208858ca42900838ab80bee6a5f981c2
mari_s <ms.mari.suzuki@gmail.com>
2020-06-29 12:19:15 +0900

    added Sample7.java (Clicking the button displays an alert message)

---
+++ src/sample/Sample7.java
@@ -0,0 +1,52 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Sample7 extends Application {
+    // clicking the button displays an alert
+    private Label lb;
+    private Button bt;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start (Stage stage) throws Exception {
+        lb = new Label("いらっしゃいませ");
+        bt = new Button("購入");
+
+        BorderPane bp = new BorderPane();
+
+        bp.setTop(lb);
+        bp.setCenter(bt);
+
+        bt.setOnAction(new SampleEventHandler());
+
+        Scene sc = new Scene(bp, 300, 200);
+
+        stage.setScene(sc);
+
+        stage.setTitle("サンプル");
+        stage.show();
+    }
+
+    class SampleEventHandler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent e) {
+            Alert al = new Alert(Alert.AlertType.INFORMATION);
+            al.setTitle("購入");
+            al.getDialogPane().setHeaderText("ご購入ありがとうございました。");
+            al.show();
+        }
+    }
+
+
+
+}
